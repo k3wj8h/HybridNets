284f8492bc21fa0beaaeff491b6776f09e0c6d04
k3wj8h <bence.kataipal@gmail.com>
2022-05-25 18:25:15 +0200

    Add files via upload

--- hybridnets/utils.py
+++ hybridnets/utils.py
@@ -7,134 +7,134 @@ import matplotlib.pyplot as plt
 
 class BBoxTransform(nn.Module):
 
-    def forward(self, anchors, regression):
-        y_centers_a = (anchors[..., 0] + anchors[..., 2]) / 2
-        x_centers_a = (anchors[..., 1] + anchors[..., 3]) / 2
-        ha = anchors[..., 2] - anchors[..., 0]
-        wa = anchors[..., 3] - anchors[..., 1]
+	def forward(self, anchors, regression):
+		y_centers_a = (anchors[..., 0] + anchors[..., 2]) / 2
+		x_centers_a = (anchors[..., 1] + anchors[..., 3]) / 2
+		ha = anchors[..., 2] - anchors[..., 0]
+		wa = anchors[..., 3] - anchors[..., 1]
 
-        w = regression[..., 3].exp() * wa
-        h = regression[..., 2].exp() * ha
+		w = regression[..., 3].exp() * wa
+		h = regression[..., 2].exp() * ha
 
-        y_centers = regression[..., 0] * ha + y_centers_a
-        x_centers = regression[..., 1] * wa + x_centers_a
+		y_centers = regression[..., 0] * ha + y_centers_a
+		x_centers = regression[..., 1] * wa + x_centers_a
 
-        ymin = y_centers - h / 2.
-        xmin = x_centers - w / 2.
-        ymax = y_centers + h / 2.
-        xmax = x_centers + w / 2.
+		ymin = y_centers - h / 2.
+		xmin = x_centers - w / 2.
+		ymax = y_centers + h / 2.
+		xmax = x_centers + w / 2.
 
-        return torch.stack([xmin, ymin, xmax, ymax], dim=2)
+		return torch.stack([xmin, ymin, xmax, ymax], dim=2)
 
 
 class ClipBoxes(nn.Module):
 
-    def __init__(self):
-        super(ClipBoxes, self).__init__()
+	def __init__(self):
+		super(ClipBoxes, self).__init__()
 
-    def forward(self, boxes, img):
-        batch_size, num_channels, height, width = img.shape
+	def forward(self, boxes, img):
+		batch_size, num_channels, height, width = img.shape
 
-        boxes[:, :, 0] = torch.clamp(boxes[:, :, 0], min=0)
-        boxes[:, :, 1] = torch.clamp(boxes[:, :, 1], min=0)
+		boxes[:, :, 0] = torch.clamp(boxes[:, :, 0], min=0)
+		boxes[:, :, 1] = torch.clamp(boxes[:, :, 1], min=0)
 
-        boxes[:, :, 2] = torch.clamp(boxes[:, :, 2], max=width - 1)
-        boxes[:, :, 3] = torch.clamp(boxes[:, :, 3], max=height - 1)
+		boxes[:, :, 2] = torch.clamp(boxes[:, :, 2], max=width - 1)
+		boxes[:, :, 3] = torch.clamp(boxes[:, :, 3], max=height - 1)
 
-        return boxes
+		return boxes
 
 
 def postprocess(x, anchors, regression, classification, regressBoxes, clipBoxes, threshold, iou_threshold):
-    transformed_anchors = regressBoxes(anchors, regression)
-    transformed_anchors = clipBoxes(transformed_anchors, x)
-    scores = torch.max(classification, dim=2, keepdim=True)[0]
-    scores_over_thresh = (scores > threshold)[:, :, 0]
-    
-    out = []
-    for i in range(x.shape[0]):
-        if scores_over_thresh[i].sum() == 0:
-            out.append({'rois': np.array(()), 'class_ids': np.array(()), 'scores': np.array(())})
-            continue
+	transformed_anchors = regressBoxes(anchors, regression)
+	transformed_anchors = clipBoxes(transformed_anchors, x)
+	scores = torch.max(classification, dim=2, keepdim=True)[0]
+	scores_over_thresh = (scores > threshold)[:, :, 0]
+	
+	out = []
+	for i in range(x.shape[0]):
+		if scores_over_thresh[i].sum() == 0:
+			out.append({'rois': np.array(()), 'class_ids': np.array(()), 'scores': np.array(())})
+			continue
 
-        classification_per = classification[i, scores_over_thresh[i, :], ...].permute(1, 0)
-        transformed_anchors_per = transformed_anchors[i, scores_over_thresh[i, :], ...]
-        scores_per = scores[i, scores_over_thresh[i, :], ...]
-        scores_, classes_ = classification_per.max(dim=0)
-        anchors_nms_idx = batched_nms(transformed_anchors_per, scores_per[:, 0], classes_, iou_threshold=iou_threshold)
+		classification_per = classification[i, scores_over_thresh[i, :], ...].permute(1, 0)
+		transformed_anchors_per = transformed_anchors[i, scores_over_thresh[i, :], ...]
+		scores_per = scores[i, scores_over_thresh[i, :], ...]
+		scores_, classes_ = classification_per.max(dim=0)
+		anchors_nms_idx = batched_nms(transformed_anchors_per, scores_per[:, 0], classes_, iou_threshold=iou_threshold)
 
-        if anchors_nms_idx.shape[0] != 0:
-            classes_ = classes_[anchors_nms_idx]
-            scores_ = scores_[anchors_nms_idx]
-            boxes_ = transformed_anchors_per[anchors_nms_idx, :]
+		if anchors_nms_idx.shape[0] != 0:
+			classes_ = classes_[anchors_nms_idx]
+			scores_ = scores_[anchors_nms_idx]
+			boxes_ = transformed_anchors_per[anchors_nms_idx, :]
 
-            out.append({'rois': boxes_.cpu().numpy(), 'class_ids': classes_.cpu().numpy(), 'scores': scores_.cpu().numpy()})
-        else:
-            out.append({'rois': np.array(()), 'class_ids': np.array(()), 'scores': np.array(())})
+			out.append({'rois': boxes_.cpu().numpy(), 'class_ids': classes_.cpu().numpy(), 'scores': scores_.cpu().numpy()})
+		else:
+			out.append({'rois': np.array(()), 'class_ids': np.array(()), 'scores': np.array(())})
 
-    return out
+	return out
 
 
 def scale_coords(img1_shape, coords, img0_shape, ratio_pad=None):
-    if len(coords) == 0:
-        return []
-    # Rescale coords (xyxy) from img1_shape to img0_shape
-    if ratio_pad is None:  # calculate from img0_shape
-        gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
-        pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
-    else:
-        gain = ratio_pad[0][0]
-        pad = ratio_pad[1]
+	if len(coords) == 0:
+		return []
+	# Rescale coords (xyxy) from img1_shape to img0_shape
+	if ratio_pad is None:  # calculate from img0_shape
+		gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
+		pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
+	else:
+		gain = ratio_pad[0][0]
+		pad = ratio_pad[1]
 
-    coords[:, [0, 2]] -= pad[0]  # x padding
-    coords[:, [1, 3]] -= pad[1]  # y padding
-    coords[:, :4] /= gain
-    clip_coords(coords, img0_shape)
-    return coords
+	coords[:, [0, 2]] -= pad[0]  # x padding
+	coords[:, [1, 3]] -= pad[1]  # y padding
+	coords[:, :4] /= gain
+	clip_coords(coords, img0_shape)
+	return coords
 
 
 def clip_coords(boxes, shape):
-    # Clip bounding xyxy bounding boxes to image shape (height, width)
-    if isinstance(boxes, torch.Tensor):  # faster individually
-        boxes[:, 0].clamp_(0, shape[1])  # x1
-        boxes[:, 1].clamp_(0, shape[0])  # y1
-        boxes[:, 2].clamp_(0, shape[1])  # x2
-        boxes[:, 3].clamp_(0, shape[0])  # y2
-    else:  # np.array (faster grouped)
-        boxes[:, [0, 2]] = boxes[:, [0, 2]].clip(0, shape[1])  # x1, x2
-        boxes[:, [1, 3]] = boxes[:, [1, 3]].clip(0, shape[0])  # y1, y2
+	# Clip bounding xyxy bounding boxes to image shape (height, width)
+	if isinstance(boxes, torch.Tensor):  # faster individually
+		boxes[:, 0].clamp_(0, shape[1])  # x1
+		boxes[:, 1].clamp_(0, shape[0])  # y1
+		boxes[:, 2].clamp_(0, shape[1])  # x2
+		boxes[:, 3].clamp_(0, shape[0])  # y2
+	else:  # np.array (faster grouped)
+		boxes[:, [0, 2]] = boxes[:, [0, 2]].clip(0, shape[1])  # x1, x2
+		boxes[:, [1, 3]] = boxes[:, [1, 3]].clip(0, shape[0])  # y1, y2
 
 
 def plot_one_box(img, coord, label=None, score=None, color=None, line_thickness=None):
-    tl = line_thickness or int(round(0.001 * max(img.shape[0:2])))  # line thickness
-    c1, c2 = (int(coord[0]), int(coord[1])), (int(coord[2]), int(coord[3]))
-    cv2.rectangle(img, c1, c2, color, thickness=tl)
-    if label:
-        tf = max(tl - 2, 1)  # font thickness
-        s_size = cv2.getTextSize(str('{:.0%}'.format(score)), 0, fontScale=float(tl) / 3, thickness=tf)[0]
-        t_size = cv2.getTextSize(label, 0, fontScale=float(tl) / 3, thickness=tf)[0]
-        c2 = c1[0] + t_size[0] + s_size[0] + 15, c1[1] - t_size[1] - 3
-        cv2.rectangle(img, c1, c2, color, -1)  # filled
-        cv2.putText(img, '{}: {:.0%}'.format(label, score), (c1[0], c1[1] - 2), 0, float(tl) / 3, [0, 0, 0], thickness=tf, lineType=cv2.FONT_HERSHEY_SIMPLEX)
+	tl = line_thickness or int(round(0.001 * max(img.shape[0:2])))  # line thickness
+	c1, c2 = (int(coord[0]), int(coord[1])), (int(coord[2]), int(coord[3]))
+	cv2.rectangle(img, c1, c2, color, thickness=tl)
+	if label:
+		tf = max(tl - 2, 1)  # font thickness
+		s_size = cv2.getTextSize(str('{:.0%}'.format(score)), 0, fontScale=float(tl) / 3, thickness=tf)[0]
+		t_size = cv2.getTextSize(label, 0, fontScale=float(tl) / 3, thickness=tf)[0]
+		c2 = c1[0] + t_size[0] + s_size[0] + 15, c1[1] - t_size[1] - 3
+		cv2.rectangle(img, c1, c2, color, -1)  # filled
+		cv2.putText(img, '{}: {:.0%}'.format(label, score), (c1[0], c1[1] - 2), 0, float(tl) / 3, [0, 0, 0], thickness=tf, lineType=cv2.FONT_HERSHEY_SIMPLEX)
 
 
 def save_checkpoint(model_with_loss, saved_path, name, optimizer=None, scheduler=None, step=None, results=None):
-    ckpt_obj = {}
-    ckpt_obj['model'] = model_with_loss.model.state_dict()
-    ckpt_obj['optimizer'] = optimizer.state_dict() if optimizer else None
+	ckpt_obj = {}
+	ckpt_obj['model'] = model_with_loss.model.state_dict()
+	ckpt_obj['optimizer'] = optimizer.state_dict() if optimizer else None
 	ckpt_obj['scheduler'] = scheduler.state_dict() if scheduler else None
-    ckpt_obj['step'] = step if step else 0
-    ckpt_obj['results'] = results.to_json(orient='columns') if isinstance(results, pd.DataFrame) else None
-    filename = os.path.join(saved_path, name)
-    torch.save(ckpt_obj, filename)
+	ckpt_obj['step'] = step if step else 0
+	ckpt_obj['results'] = results.to_json(orient='columns') if isinstance(results, pd.DataFrame) else None
+	filename = os.path.join(saved_path, name)
+	torch.save(ckpt_obj, filename)
 
 
 def save_image(image, filename, image_path='./sample_images/', download=False, figsize=(15,9)):
-    os.makedirs(image_path, exist_ok=True)
+	os.makedirs(image_path, exist_ok=True)
 
-    fig, ax = plt.subplots(1,1,figsize=figsize)
-    ax.imshow(image, interpolation='nearest', aspect='auto')
-    filename = os.path.join(image_path, filename)
-    fig.savefig(filename)
-    plt.close()
+	fig, ax = plt.subplots(1,1,figsize=figsize)
+	ax.imshow(image, interpolation='nearest', aspect='auto')
+	filename = os.path.join(image_path, filename)
+	fig.savefig(filename)
+	plt.close()
 
 
